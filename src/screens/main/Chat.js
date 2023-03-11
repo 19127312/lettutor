6fc9b1c8bb22eef510f58bea9ee867e701ffa83c
19127312 <19127312@student.hcmus.edu.vn>
2023-03-11 22:11:18 +0700

    Connect ChatGPT API

--- src/screens/main/Chat.js
+++ src/screens/main/Chat.js
@@ -8,78 +8,52 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   FlatList,
-  Dimensions,
 } from "react-native";
 import { Icon } from "react-native-elements";
+import ChatItem from "../../components/ChatItem";
+import axios from "axios";
+import { REACT_APP_API_KEY } from "@env";
 
 export default function ChatScreen1() {
-  const [currentUser] = useState({
-    name: "John Doe",
-  });
-
-  const [messages, setMessages] = useState([
-    { sender: "John Doe", message: "Hey there!", time: "6:01 PM" },
-    {
-      sender: "Robert Henry",
-      message: "Hello, how are you doing?",
-      time: "6:02 PM",
-    },
-    {
-      sender: "John Doe",
-      message: "I am good, how about you?",
-      time: "6:02 PM",
-    },
-    {
-      sender: "John Doe",
-      message: `😊😇`,
-      time: "6:02 PM",
-    },
-    {
-      sender: "Robert Henry",
-      message: `Can't wait to meet you.`,
-      time: "6:03 PM",
-    },
-    {
-      sender: "John Doe",
-      message: `That's great, when are you coming?`,
-      time: "6:03 PM",
-    },
-    {
-      sender: "Robert Henry",
-      message: `This weekend.`,
-      time: "6:03 PM",
-    },
-    {
-      sender: "Robert Henry",
-      message: `Around 4 to 6 PM.`,
-      time: "6:04 PM",
-    },
-    {
-      sender: "John Doe",
-      message: `Great, don't forget to bring me some mangoes.`,
-      time: "6:05 PM",
-    },
-    {
-      sender: "Robert Henry",
-      message: `Sure!`,
-      time: "6:05 PM",
-    },
-  ]);
+  const [messages, setMessages] = useState([]);
 
   const [inputMessage, setInputMessage] = useState("");
 
-  function sendMessage() {
+  async function sendMessage() {
     if (inputMessage === "") {
       return setInputMessage("");
     }
-    setMessages([
-      ...messages,
-      {
-        sender: currentUser.name,
-        message: inputMessage,
-      },
-    ]);
+    setMessages((pre) => {
+      return [...pre, { message: inputMessage, fromMe: true }];
+    });
     setInputMessage("");
+
+    const apikey = REACT_APP_API_KEY;
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      {
+        prompt: inputMessage,
+        max_tokens: 60,
+        temperature: 0.7,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        model: "text-davinci-003",
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${apikey}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const res = response.data.choices[0].text;
+    let mess = res.replace(/^\n/, "");
+    mess = mess.replace(/^\n/, "");
+
+    setMessages((pre) => {
+      return [...pre, { message: mess, fromMe: false }];
+    });
   }
 
   return (
@@ -89,38 +63,7 @@ export default function ChatScreen1() {
           style={{ backgroundColor: "#f2f2ff" }}
           inverted={true}
           data={JSON.parse(JSON.stringify(messages)).reverse()}
-          renderItem={({ item }) => (
-            <TouchableWithoutFeedback>
-              <View style={{ marginTop: 6 }}>
-                <View
-                  style={{
-                    maxWidth: Dimensions.get("screen").width * 0.8,
-                    backgroundColor: "#3a6ee8",
-                    alignSelf:
-                      item.sender === currentUser.name
-                        ? "flex-end"
-                        : "flex-start",
-                    marginHorizontal: 10,
-                    padding: 10,
-                    borderRadius: 8,
-                    borderBottomLeftRadius:
-                      item.sender === currentUser.name ? 8 : 0,
-                    borderBottomRightRadius:
-                      item.sender === currentUser.name ? 0 : 8,
-                  }}
-                >
-                  <Text
-                    style={{
-                      color: "#fff",
-                      fontSize: 16,
-                    }}
-                  >
-                    {item.message}
-                  </Text>
-                </View>
-              </View>
-            </TouchableWithoutFeedback>
-          )}
+          renderItem={({ item }) => <ChatItem item={item} />}
         />
 
         <View style={{ paddingVertical: 10 }}>
@@ -150,14 +93,6 @@ export default function ChatScreen1() {
 }
 
 const styles = StyleSheet.create({
-  headerLeft: {
-    paddingVertical: 4,
-    paddingHorizontal: 10,
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  userProfileImage: { height: "100%", aspectRatio: 1, borderRadius: 100 },
   container: {
     flex: 1,
     backgroundColor: "#f2f2ff",
